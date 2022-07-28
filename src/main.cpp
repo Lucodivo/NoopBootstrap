92d7e4c1e2ca23790fd8e6b5fc1352d42452049b
Connor Haskins <ConnorAHaskins@gmail.com>
2022-07-27 21:24:38 -0700

    Render with OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,39 @@
 #include <SDL.h>
+#include <SDL_opengl.h>
 
 #include "glm/vec3.hpp"
 
 #include "types.h"
+
+#define INIT_WINDOW_WIDTH 640
+#define INIT_WINDOW_HEIGHT 480
+
+SDL_Window* initSDL(){
+  SDL_Window* window = SDL_CreateWindow(
+          "bootstrap",
+          SDL_WINDOWPOS_UNDEFINED,
+          SDL_WINDOWPOS_UNDEFINED,
+          INIT_WINDOW_WIDTH,
+          INIT_WINDOW_HEIGHT,
+          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
+  );
+  SDL_CaptureMouse(SDL_TRUE);
+  SDL_SetHintWithPriority(SDL_HINT_MOUSE_RELATIVE_MODE_WARP, "1", SDL_HINT_OVERRIDE);
+  return window;
+}
 
 int main(int argc, char* argv[]) {
   glm::vec3 screenColor = glm::vec3{0.5f, 0.0f, 0.0f};
 
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow(
-          "SDL2Test",
-          SDL_WINDOWPOS_UNDEFINED,
-          SDL_WINDOWPOS_UNDEFINED,
-          640,
-          480,
-          0
-  );
+  SDL_Window *window = initSDL();
+  SDL_GLContext context = SDL_GL_CreateContext(window);
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, u8(255 * screenColor.r), u8(255 * screenColor.g), u8(255 * screenColor.b), SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+  SDL_GL_MakeCurrent(window, context);
+  glClearColor(screenColor.r, screenColor.g, screenColor.b, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT );
+  SDL_GL_SwapWindow(window);
 
   SDL_Delay(3000);
 
